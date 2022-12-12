347357da9da1cfc2b9e8c7c4bc8e25a0e55bc130
m1sh3ll <mn218@nerdymail.com>
2022-12-11 20:29:15 -0500

    mn

---
+++ hello/hello.stub.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @generate-class-entries
+ * @undocumentable
+ */
+
+function hello_info(): void {}
+
+function hello_shout(string $str = ""): string {}
---
+++ hello/hello_arginfo.h
@@ -0,0 +1,9 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 10304ab9506dba26b0be245cec67f1d28c9159d2 */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hello_info, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hello_shout, 0, 0, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
+ZEND_END_ARG_INFO()
